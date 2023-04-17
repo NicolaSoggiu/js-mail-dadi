a32ba819c1e648f43c02bb5077e1ead9b62c3849
NicolaSoggiu <Nicola.soggiu.work@gmail.com>
2023-04-17 15:15:12 +0200

    add excercise "dice"

--- js/myscript.js
+++ js/myscript.js
@@ -1,24 +1,24 @@
 // EXCERCISE MAIL
-// let email = prompt("Enter your email to verify access");
-// console.log(email);
+let email = prompt("Enter your email to verify access");
+console.log(email);
 
-// // ARRAY
-// const accessList = [
-//   "nicola.soggiu@gmail.com",
-//   "mario.rossi@gmail.com",
-//   "paolo.verdi@gmail.com",
-//   "alessandro.conte@gmail.com",
-//   "filippo.rinascente@gmail.com",
-// ];
-// console.log(accessList);
+// ARRAY
+const accessList = [
+  "nicola.soggiu@gmail.com",
+  "mario.rossi@gmail.com",
+  "paolo.verdi@gmail.com",
+  "alessandro.conte@gmail.com",
+  "filippo.rinascente@gmail.com",
+];
+console.log(accessList);
 
-// if (email == "nicola.soggiu@gmail.com") {
-//   alert("Login successful!");
-//   console.log("Login successful!");
-// } else {
-//   alert("Access denied!");
-//   console.log("Access denied!");
-// }
+if (email == "nicola.soggiu@gmail.com") {
+  alert("Login successful!");
+  console.log("Login successful!");
+} else {
+  alert("Access denied!");
+  console.log("Access denied!");
+}
 
 // EXCERCISE DICE
 
